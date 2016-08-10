9070957abacf5ec190167162e9e582fd68d17fab
Patty Kovash <pattykovash@gmail.com>
2016-08-10 11:45:39 -0600

    Added other draw functions along with a rotate function.

--- shapes.py
+++ shapes.py
@@ -1,10 +1,11 @@
-# Draw geometric shape out of squares
+# Draw geometric shapes
 
 # By PK
 # Udacity Programming Foundations
 # August 2016
 
 import turtle
+
 
 def draw_square(turt, width):
 	"""
@@ -15,34 +16,70 @@ def draw_square(turt, width):
 		turt.forward(width)
 		turt.right(90)
 
+def draw_triangle(turt, side_len):
+	"""
+	Takes a Turtle object and int as an argument.
+	Draws 1 equilateral triangle with side_len length. 
+	"""
+
+	for i in range(3):
+		turt.forward(side_len)
+		turt.right(120)
+
+def draw_circle(turt, rad):
+	"""
+	Takes a Turtle object and draws a circle with radius rad.
+	"""
+	turt.circle(rad)
+
+def rotate(turt, draw_func, size, rot_num):
+	"""
+	Takes as arguments a Turtle object, draw function, int for size, 
+	and int between 1 - 36 rep. number of rotations. Draws shape and
+	rotates Turtle object rot_num times. 
+	"""
+	for i in range(rot_num):
+		draw_func(turt, size)
+		turt.left(10)
+		
 def draw():
 	"""
-	Draws a geometric shape out of squares
+	Draws a geometric shape.
 	"""
 
 	WIDTH = 400
 	HEIGHT = 400
-	SPEED = 6
+	SPEED = 10
 
 
 	frame = turtle.Screen()
 	frame.setup(WIDTH, HEIGHT)
 	frame.bgcolor("grey")
 
-	# square turtle
-	square = turtle.Turtle()
-	square.color("white", "white")
-	square.speed(SPEED)
-	square.fill(True)
-	square.setposition(0, 0)
+	# # square turtle
+	# square = turtle.Turtle()
+	# square.color("white", "white")
+	# square.speed(SPEED)
+	# square.fill(True)
+	# square.setposition(0, 0)
+	# rotate(square, draw_square, 100, 36)
 
+	# # triangle turtle
+	# triangle = turtle.Turtle()
+	# triangle.color("white", "white")
+	# triangle.speed(SPEED)
+	# triangle.setposition(0, 0)
+	# rotate(triangle, draw_triangle, 100, 36)
 
-	for i in range(36):
-		draw_square(square, 200)
-		square.right(10)
+	# circle turtle
+	circle = turtle.Turtle()
+	circle.color("white", "white")
+	circle.speed(SPEED)
+	circle.fill(True)
+	circle.setposition(0, 0)
+	rotate(circle, draw_circle, 100, 36)
 
-
-	square.fill(False)	
+	# square.fill(False)	
 	frame.exitonclick()
 
 draw()
